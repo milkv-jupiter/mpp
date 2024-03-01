1f9aee5123169143e944a9b3d5f860a805074c12
James Deng <james.deng@spacemit.com>
2024-03-01 19:30:01 +0800

    Update for v1.0alpha2

---
+++ al/v4l2/linlonv5v7/include/linlonv5v7_constant.h
@@ -0,0 +1,28 @@
+/***
+ * @Copyright 2022-2023 SPACEMIT. All rights reserved.
+ * @Use of this source code is governed by a BSD-style license
+ * @that can be found in the LICENSE file.
+ * @
+ * @Author: David(qiang.fu@spacemit.com)
+ * @Date: 2023-12-30 10:48:27
+ * @LastEditTime: 2023-12-30 11:02:47
+ * @Description:
+ */
+
+#ifndef _LINLONV5V7_CONSTANT_H_
+#define _LINLONV5V7_CONSTANT_H_
+
+#define MAX_BUF_NUM (32)
+#define MAX_INPUT_BUF_NUM (32)
+#define MAX_OUTPUT_BUF_NUM (32)
+
+#define INPUT_BUF_NUM (12)
+#define OUTPUT_BUF_NUM (8)
+
+#define POLL_TIMEOUT (0)
+
+#define SIZE_IMAGE (1024 * 1024)
+
+#define LINLON_FILE_NAME_LEN (512)
+
+#endif /*_LINLONV5V7_CONSTANT_H_*/
